2372c08431c67417983d100b9c143480bb3d8dc7
Gayatri Munde <gayatri.munde19@vit.edu>
2021-08-28 14:33:14 +0530

    721

--- LeetCode/Strings/3_LongestSubstringWithoutRepeatingCharacters.cpp
+++ LeetCode/Strings/3_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,6 +1,8 @@
 //Que: 
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+
+//Approch 1: Brute force
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -18,5 +20,24 @@ public:
         }
         
         return maxCount;
+    }
+};
+
+//Approach 2: Sliding Window
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int start = 0, ptr = 0;
+        int count = 0;
+        int hashmap[128] = {0};
+        
+        while(ptr < s.length()){
+            start = max(hashmap[s[ptr]], start);
+            count = max(count, ptr - start + 1);
+            hashmap[s[ptr]] = ptr + 1;
+            ptr++;
+        }
+        
+        return count;
     }
 };
